56619b9a90f73e05861d77ec229e6257ab2b3d68
NathanW0909 <nawong8@s.sfusd.edu>
2025-04-25 23:44:15 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,7 +1,27 @@
 import java.util.*;
 public class RandomStringChooser
 {
-  /* to be implemented in part (a) */
+ private String [] wordArray; 
+private ArrayList <Integer> uncalledIndex; 
+public RandomStringChooser (String [] s){
+  uncalledIndex = new ArrayList <Integer>(); 
+  wordArray = s; 
+  
+  for (int i = 0; i< wordArray.length; i++){
+    uncalledIndex.add(i);
+  }
+}
+  
+  public String getNext(){
+    if(uncalledIndex.size() == 0){
+      return ("NONE");
+    }
+    
+    int x = (int)(Math.random()*uncalledIndex.size());
+    String s = wordArray[uncalledIndex.get(x)];
+    uncalledIndex.remove(x); 
+    return s; 
+  }
   
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
